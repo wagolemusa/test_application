b305f4ba7f05c64a904b7bb49358c8e1b8d527ec
wagole musa <homiemusa@gmail.com>
2017-07-08 10:43:52 +0300

    Add admin user functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -80,7 +80,8 @@ end
 	end
 
 	def require_same_user
-		if current_user != @article.user
+		#if current_user != @article.user
+		if current_user != @article.user  and !current_user.admin? #it adds admin 
 			flash[:danger] = "You can only edit or delete your own articles"
 			redirect_to root_path
 		end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 	before_action :set_user, only: [:edit, :update, :show]
-	before_action :require_same_user, only: [:edit, :update] # it rectrite user to edit or update others user'accounts
+	before_action :require_same_user, only: [:edit, :update, :destroy] # it rectrite user to edit or update others user'accounts
+	before_action :require_admin, only: [:destroy] #it unables admin to  delete userss
 
 	def index
 		#@users = User.all 
@@ -16,8 +17,9 @@ def create
 	#debugger
 	@user = User.new(user_params)
 	if @user.save
+		session[:user_id] = @user.id  # signup fuctions for users 
 		flash[:success] = "Welcome to the alpha blog #{@user.username}"
-		redirect_to articles_path
+		redirect_to user_path(@user)
 	else
 		render 'new'
 	end	
@@ -44,6 +46,13 @@ def show
 	@user_articles = @user.articles.paginate(page: params[:page], per_page: 5) 	
 end
 
+def destroy  #this function will heilp admin to delete a user
+	@user = User.find(params[:id])
+	@user.destroy
+	flash[:danger] = "User and all articles created by user have been delete"
+	redirect_to users_path	
+end
+
 
 private
 
@@ -57,8 +66,16 @@ def set_user
 end
 
 def require_same_user
-	if current_user != @user 
+	#if current_user != @user
+	if current_user != @user and !current_user.admin?  #it unable admin to delete users 
 		flash[:danger] = "You can only edit your own account"
+		redirect_to root_path
+	end
+end
+
+def require_admin # unable admi  to delete users
+	if logged_in? and !current_user.admin?
+		flash[:danger] = "only admin can perform that action"
 		redirect_to root_path
 	end
 end
